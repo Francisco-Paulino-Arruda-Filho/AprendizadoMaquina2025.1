7e15c1f6caaa8899d22e564240ffbdffce4db68a
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2025-04-25 21:27:20 -0300

    inicio capitulo 2 do livro

--- IrisDataSet/index.py
+++ IrisDataSet/index.py
@@ -1,5 +1,18 @@
-with open('iris.csv', 'r', encoding='utf-8') as f:
-    iris = f.readlines()
-iris = [line.strip().split(',') for line in iris[1:]]
-    
-print(iris)
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+dataset = pd.read_csv("Iris.csv")
+
+X_train, X_test, y_train, y_test = train_test_split(
+    dataset.drop(columns=["Species"]),
+    dataset["Species"],
+    test_size=0.2,
+    random_state=42,
+)
+
+X_train = pd.get_dummies(X_train, drop_first=True)
+X_test = pd.get_dummies(X_test, drop_first=True)
+X_train = X_train.values
+X_test = X_test.values
+y_train = y_train.values
+y_test = y_test.values
